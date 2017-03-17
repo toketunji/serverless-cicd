7a944ef9030b36247a82817169ee3475f500dc53
Imran <imuzzz@gmail.com>
2017-03-17 14:36:56 +0000

    add logging

--- test/env.js
+++ test/env.js
@@ -3,6 +3,8 @@ function init(){
   process.env.USER_TABLE = `serverless-sample-user-${process.env.AWS_STAGE}`;
 
   process.env.URL = `https://tl0orf5x0a.execute-api.eu-west-1.amazonaws.com/${process.env.AWS_STAGE}`;
+
+  console.log('api url: ', process.env.URL);
 }
 
 module.exports = {

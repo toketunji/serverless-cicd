3701c8adc85c3aaddc78fa983dfbbdf24639a9aa
Imran <imuzzz@gmail.com>
2017-03-17 13:40:52 +0000

    working integration test

--- test/env.js
+++ test/env.js
@@ -2,7 +2,7 @@ function init(){
   require('aws-sdk').config.region = process.env.AWS_REGION;
   process.env.USER_TABLE = `serverless-sample-user-${process.env.AWS_STAGE}`;
 
-  process.env.URL = `https://5atiwoggt5.execute-api.eu-west-1.amazonaws.com/${process.env.AWS_STAGE}`;
+  process.env.URL = `https://tl0orf5x0a.execute-api.eu-west-1.amazonaws.com/${process.env.AWS_STAGE}`;
 }
 
 module.exports = {

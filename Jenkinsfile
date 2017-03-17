15d544d829b6de34f7b976e5806cfadb7ac18d4a
Imran <imuzzz@gmail.com>
2017-03-17 10:04:51 +0000

    jenkins

---
+++ Jenkinsfile
@@ -0,0 +1,21 @@
+#!groovy
+
+pipeline {
+    agent any 
+
+    stages {
+        stage('Build') { 
+            steps { 
+                sh 'echo hello' 
+            }
+        }
+        stage('Test'){
+            steps {
+            }
+        }
+        stage('Deploy') {
+            steps {
+            }
+        }
+    }
+}
--- test/env.js
+++ test/env.js
@@ -1,10 +1,8 @@
 function init(){
-  process.env.AWS_REGION = 'eu-west-1';
   require('aws-sdk').config.region = process.env.AWS_REGION;
-  process.env.AWS_STAGE = 'dev';
   process.env.USER_TABLE = `serverless-sample-user-${process.env.AWS_STAGE}`;
 
-  process.env.URL = `https://5atiwoggt5.execute-api.eu-west-1.amazonaws.com/${process.env.STAGE}`;
+  process.env.URL = `https://5atiwoggt5.execute-api.eu-west-1.amazonaws.com/${process.env.AWS_STAGE}`;
 }
 
 module.exports = {
